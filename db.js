8d450c09e3900915b844d2b226c6eede35b22647
Priyanshu <133294909+Priyanshugola@users.noreply.github.com>
2025-11-27 21:20:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,22 @@
+// const mongoose = require('mongoose');
+// const mongouri = "mongodb://localhost:27017/user_master?directConnection=true";
+
+// const conn = async () => {
+//   try {
+//     await mongoose.connect(mongouri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log('✅ MongoDB Atlas connected');
+//   } catch (err) {
+//     console.error('❌ Connection error:', err);
+//   }
+// };
+// module.exports = conn;
 const mongoose = require('mongoose');
-const mongouri = "mongodb://localhost:27017/user_master?directConnection=true";
+
+// ✅ MongoDB Atlas ka connection URI yaha paste karo
+const mongouri = "mongodb+srv://priyanshu306study_db_user:DLvk6HjuzYzrZuTq@gnote.gtdtupn.mongodb.net/user_master?retryWrites=true&w=majority";
 
 const conn = async () => {
   try {
@@ -7,9 +24,11 @@ const conn = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log('✅ MongoDB Atlas connected');
+    console.log('✅ MongoDB Atlas Connected Successfully');
   } catch (err) {
-    console.error('❌ Connection error:', err);
+    console.error('❌ MongoDB Connection Error:', err);
   }
 };
+
 module.exports = conn;
+
