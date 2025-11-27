c1acd6e9f90b366a871a139c922b587481bd2917
Priyanshu <133294909+Priyanshugola@users.noreply.github.com>
2025-11-27 22:10:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -18,7 +18,7 @@ const mongoose = require('mongoose');
 // ✅ MongoDB Atlas ka connection URI yaha paste karo
 // const mongouri = "mongodb+srv://priyanshu306study_db_user:DLvk6HjuzYzrZuTq@gnote.gtdtupn.mongodb.net/user_master?retryWrites=true&w=majority";
 // const mongouri = "mongodb+srv://priyanshu306study_db_user:DLvk6HjuzYzrZuTq@gnote.gtdtupn.mongodb.net/user_master?retryWrites=true&w=majority";
-const mongouri = "mongodb+srv://priyanshu306study_db_user:DLvk6HjuzYzrZuTq@gnote.gtdtupn.mongodb.net/user_master?retryWrites=true&w=majority";
+const mongouri = process.env.MONGO_URI;
 
 const conn = async () => {
   try {
