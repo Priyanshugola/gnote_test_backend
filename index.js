f598375068819f73851f255e51bca60919f5524b
Priyanshu <133294909+Priyanshugola@users.noreply.github.com>
2025-11-27 12:15:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+// console.log("ss");
+// // const conn = require('./db.js');
+// const express = require('express');
+// var cors = require('cors')
+// // conn();
+// const app = express();
+// app.use(cors())
+// exports.app = app;
+// const port = 5000; 
+// // app.get('/',(req, res)=>{
+// //     res.send('hello world')
+// // })
+// app.use(express.json())
+// app.use("/", require('./router/Auth'));
+// app.use("/", require('./router/Note'));
+// app.use("/", (req, res, next) => {
+//   console.log("ss");
+//   next(); // Continue to the next middleware
+// });
+// // app.listen(port,()=>{
+// //     console.log(`Example port on http://localhost:${port}`)
+// // })d
+// // export default app;
+// exports.app = app;
 console.log("ss");
 // const conn = require('./db.js');
 const express = require('express');
@@ -5,7 +29,7 @@ var cors = require('cors')
 // conn();
 const app = express();
 app.use(cors())
-exports.app = app;
+// exports.app = app; ❌ Vercel does NOT accept this
 const port = 5000; 
 // app.get('/',(req, res)=>{
 //     res.send('hello world')
@@ -21,4 +45,7 @@ app.use("/", (req, res, next) => {
 //     console.log(`Example port on http://localhost:${port}`)
 // })d
 // export default app;
-exports.app = app;
+
+// ✔ Correct export for Vercel:
+module.exports = app;
+
