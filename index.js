9004616efc789ed34ca48b10c0a90df5ae56b95d
Priyanshu <133294909+Priyanshugola@users.noreply.github.com>
2025-11-27 12:25:46 +0530

    Refactor server setup and logging
    Refactor server initialization and logging. Update CORS usage and modify response messages.

--- index.js
+++ index.js
@@ -22,25 +22,37 @@
 // // })d
 // // export default app;
 // exports.app = app;
-console.log("server loaded");  // runs when server initializes
-
-console.log("server loaded");
-
+console.log("ss");
+// const conn = require('./db.js');
 const express = require('express');
-const cors = require('cors');
+var cors = require('cors')
+// conn();
 const app = express();
+app.use(cors())
+const port = 5000; 
+// app.get('/',(req, res)=>{
+//     res.send('hello world')
+// })
+app.use(express.json())
 
-app.use(cors());
-app.use(express.json());
-
-// INDEX log
+// INDEX route + console log
 app.get("/", (req, res) => {
-  console.log("Index route hit");
-  res.send("Backend working!");
+  console.log("Index route hit");     // <-- YOU WANTED THIS
+  res.send("Backend is working root index.js !");
 });
 
-app.use("/", require("/router/Auth"));
-app.use("/", require("/router/Note"));
+app.use("/", require('./router/Auth'));
+app.use("/", require('./router/Note'));
 
+app.use("/", (req, res, next) => {
+  console.log("ss");
+  next(); // Continue to the next middleware
+});
+
+// app.listen(port,()=>{
+//     console.log(`Example port on http://localhost:${port}`)
+// })d
+// export default app;
+
+// IMPORTANT FOR VERCEL:
 module.exports = app;
-
