904c844186cc0360dc845d5cf7902d76f4ebe06f
Priyanshu <133294909+Priyanshugola@users.noreply.github.com>
2025-11-27 12:08:33 +0530

    Refactor Express app initialization in index.js
    Uncommented and updated Express app setup code.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-console.log("ss");
+// console.log("ss");
 // const conn = require('./db.js');
-// const express = require('express');
-// var cors = require('cors')
+const express = require('express');
+var cors = require('cors')
 // conn();
-// const app = express();
-// app.use(cors())
-// exports.app = app;
-// const port = 5000; 
-// // app.get('/',(req, res)=>{
-// //     res.send('hello world')
-// // })
-// app.use(express.json())
-// app.use("/", require('./router/Auth'));
-// app.use("/", require('./router/Note'));
+const app = express();
+app.use(cors())
+exports.app = app;
+const port = 5000; 
+// app.get('/',(req, res)=>{
+//     res.send('hello world')
+// })
+app.use(express.json())
+app.use("/", require('./router/Auth'));
+app.use("/", require('./router/Note'));
 // app.listen(port,()=>{
 //     console.log(`Example port on http://localhost:${port}`)
-// })
+// })d
+export default app;
